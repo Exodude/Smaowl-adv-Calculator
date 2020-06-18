c9b3d97add497b47293cf35ed9cf74bc143563ab
Exodude <67016119+Exodude@users.noreply.github.com>
2020-06-18 17:35:28 +0530

    Add files via upload

---
+++ final cal.py
@@ -0,0 +1,13 @@
+ans=int(input("Enter the calculator choice: 1=normal,2=scientifical: "))
+n=int(input("Enter the total count of numbers you want:"))
+val=0
+if (ans<1  and ans>4):
+    print("Give a number i can count, i only have 2 fingers!")
+    ans=int(input("Enter the calculator choice: 1=normal,2=scientifical: "))
+else:
+    
+    if (ans==1):
+        
+    elif (ans==2):
+        
+            
